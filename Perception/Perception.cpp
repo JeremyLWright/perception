d52aa9f587256917cd7add4b32ca936853ffed1c
Jeremy Wright <jeremy.wright@commscope.com>
2016-02-07 13:11:24 -0700

    Successfully moved a sprite.

--- Perception/Perception.cpp
+++ Perception/Perception.cpp
@@ -13,26 +13,25 @@
 
 int main()
 {
+	sf::Texture texture;
+	if (!texture.loadFromFile("assets/orange_bunny.png", sf::IntRect(10, 10, 32, 72)))
+	{
+
+		throw std::runtime_error("Unable to load sprites.");
+	}
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Perception :: Now you see me.");
 	while (window.isOpen())
 	{
 		sf::Event event;
-		sf::Texture texture;
-		if (!texture.loadFromFile("assets/orange_bunny.png", sf::IntRect(10, 10, 32, 32)))
-		{
-
-			throw std::runtime_error("Unable to load sprites.");
-		}
-		sf::Sprite sprite;
-		sprite.setTexture(texture);
 
 
 
 		while (window.pollEvent(event))
 		{
 			window.clear();
-		window.draw(sprite);
-		window.display();
 			switch (event.type)
 			{
 			case sf::Event::Closed:
@@ -46,13 +45,44 @@ int main()
 				break;
 			case sf::Event::TextEntered:
 				if (event.text.unicode < 128)
-					std::cout << "Typed: " << static_cast<char>(event.text.unicode) << '\n';
+				{
+					const auto key = static_cast<char>(event.text.unicode);
+					switch (key)
+					{
+					case 'w':
+					{
+						std::cout << "Up\n";
+						sprite.move(sf::Vector2f(0, -2));
+						break;
+					}
+					case 'a':
+					{
+						std::cout << "Left\n";
+						sprite.move(sf::Vector2f(-2, 0));
+						break;
+					}
+					case 'd':
+					{
+						std::cout << "Right\n";
+						sprite.move(sf::Vector2f(2, 0));
+						break;
+					}
+					case 's':
+						std::cout << "Down\n";
+						sprite.move(sf::Vector2f(0, 2));
+						break;
+					}
+
+
+				}
 				break;
 
 			}
+			window.draw(sprite);
+			window.display();
 		}
 	}
 
-    return 0;
+	return 0;
 }
 
