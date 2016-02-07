ea1cc1c1b98443a31283b653d90ae3ab93594486
Jeremy Wright <jeremy.wright@commscope.com>
2016-02-06 22:07:41 -0700

    Reorganized solutions.

---
+++ Perception.Test/Perception.Test.cpp
@@ -0,0 +1,12 @@
+// Perception.Test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#define CATCH_CONFIG_MAIN
+#include <catch/catch.hpp>
+
+TEST_CASE("Simple Test")
+{
+	REQUIRE(1 == 1);
+}
+
--- Perception/Perception.cpp
+++ Perception/Perception.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
-
+#include <SFML/Window.hpp>
 
 int main()
 {
+	sf::Window window(sf::VideoMode(800, 600), "Perception :: Now you see me.");
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+
     return 0;
 }
 
