c4ac920b2adf60d83c563e200f667c89f3ac788d
Jeremy Wright <jeremy.wright@commscope.com>
2016-02-07 12:49:59 -0700

    Loaded a single sprite.

--- Perception/Perception.cpp
+++ Perception/Perception.cpp
@@ -3,17 +3,36 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <stdexcept>
+#include <cassert>
 
 #include <SFML/Window.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "Perception :: Now you see me.");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Perception :: Now you see me.");
 	while (window.isOpen())
 	{
 		sf::Event event;
+		sf::Texture texture;
+		if (!texture.loadFromFile("assets/orange_bunny.png", sf::IntRect(10, 10, 32, 32)))
+		{
+
+			throw std::runtime_error("Unable to load sprites.");
+		}
+		sf::Sprite sprite;
+		sprite.setTexture(texture);
+
+
+
 		while (window.pollEvent(event))
 		{
+			window.clear();
+		window.draw(sprite);
+		window.display();
 			switch (event.type)
 			{
 			case sf::Event::Closed:
